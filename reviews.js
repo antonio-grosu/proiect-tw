6b8e8742dbb360cc627b7251a88db7547480cb8e
Grosu Antonio <contact.codevs@gmail.com>
2025-01-16 01:24:15 +0200

    done project

--- reviews.js
+++ reviews.js
@@ -2,12 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginBtn = document.getElementById("login-btn");
   const loginForm = document.querySelector(".login-form");
   const registerForm = document.querySelector(".register-form");
+  const reviewForm = document.querySelector(".review-form");
   document.querySelector(".login-success-message").classList.add("hidden");
+  document.querySelector(".review-success-message").classList.add("hidden");
   document.querySelector(".register-success-message").classList.add("hidden");
-  const localUser = localStorage.getItem("user");
-  const loginUser = async (email, password) => {
-    const payload = JSON.stringify({ email, password });
 
+  const localUser = localStorage.getItem("user");
+  reviewForm.classList.add("hidden");
+  async function loginUser(email, password) {
+    const payload = JSON.stringify({ email, password });
+    console.log(payload);
     try {
       const response = await fetch("http://localhost:3000/auth/login", {
         method: "POST",
@@ -43,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Response not OK:", response.status);
       }
     } catch (error) {}
-  };
+  }
   const registerUser = async (name, email, password) => {
     const payload = JSON.stringify({ name, email, password });
 
@@ -69,7 +73,34 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector(".error-message").classList.remove("hidden");
     }
   };
+
+  const sendReview = async (title, review, rating) => {
+    const name = JSON.parse(localUser).name;
+    const payload = JSON.stringify({ name, title, review, rating });
+    console.log(payload);
+    try {
+      const response = await fetch("http://localhost:3000/reviews", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: payload,
+      });
+      if (response.ok) {
+        document
+          .querySelector(".review-success-message")
+          .classList.remove("hidden");
+        document.getElementById("review-form-btn").classList.add("hidden");
+        setTimeout(() => {
+          window.location.reload();
+        }, 2500);
+      }
+    } catch (error) {
+      document.querySelector(".error-message").classList.remove("hidden");
+    }
+  };
   registerForm.classList.add("hidden");
+
   document
     .querySelectorAll(".success-message")
     .forEach((element) => element.classList.add("hidden"));
@@ -79,7 +110,9 @@ document.addEventListener("DOMContentLoaded", function () {
     .forEach((element) => element.classList.add("hidden"));
 
   loginForm.addEventListener("submit", (event) => {
+    event.stopPropagation();
     event.preventDefault();
+
     const email = loginForm.querySelector("input[name='login-email']").value;
     const password = loginForm.querySelector(
       "input[name='login-password']"
@@ -88,6 +121,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   registerForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    event.stopPropagation();
     const name = registerForm.querySelector(
       "input[name='register-username']"
     ).value;
@@ -99,7 +133,20 @@ document.addEventListener("DOMContentLoaded", function () {
     ).value;
     registerUser(name, email, password);
   });
+  reviewForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const title = reviewForm.querySelector("input[name='review-title']").value;
+    const review = reviewForm.querySelector(
+      "textarea[name='review-review']"
+    ).value;
+    const rating = reviewForm.querySelector(
+      "input[name='review-rating']"
+    ).value;
+    sendReview(title, review, rating);
+  });
   document.addEventListener("click", function (event) {
+    event.stopPropagation();
     if (event.target.classList.contains("btn-to-login")) {
       loginForm.classList.remove("hidden");
       registerForm.classList.add("hidden");
@@ -110,10 +157,55 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  if (localUser) {
+  document.addEventListener("click", function (event) {
+    if (event.target.id == "add-review-btn") {
+      reviewForm.classList.remove("hidden");
+    }
+  });
+  if (localUser != null) {
     document.querySelector(".account-section").classList.add("hidden");
+
+    document.querySelector(".logged-in-as").textContent = `Logged in as: ${
+      JSON.parse(localUser).name
+    }`;
+    fetch("http://localhost:3000/reviews")
+      .then((res) => res.json())
+      .then((data) => {
+        const reviewsContainer = document.querySelector(".reviews-div");
+        if (!reviewsContainer) {
+          console.error("No element with class 'reviews-container' found.");
+          return;
+        }
+        let sum = 0;
+        data.forEach((review) => {
+          sum += parseInt(review.rating);
+          const reviewElement = document.createElement("div");
+          reviewElement.classList.add("review");
+          reviewElement.innerHTML = `
+          <h3>${review.title}</h3>
+          <p>Author: ${review.name}</p>
+          <p>${review.review}</p>
+          <p>Rating: ${review.rating}</p>
+        `;
+
+          reviewsContainer.appendChild(reviewElement);
+        });
+        const averageRating = Math.round((sum / data.length) * 10) / 10;
+        document.querySelector(
+          ".reviews-title"
+        ).textContent += ` (Avg. ${averageRating})`;
+      })
+      .catch((error) => {
+        console.error("Error fetching reviews:", error);
+      });
   } else {
     document.querySelector(".account-section").classList.remove("hidden");
     document.querySelector(".review-section").classList.add("hidden");
   }
+  document.addEventListener("click", function (event) {
+    if (event.target.id == "log-out-btn") {
+      localStorage.clear();
+      window.location.reload();
+    }
+  });
 });
